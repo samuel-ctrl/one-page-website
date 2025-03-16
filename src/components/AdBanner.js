52fa690c4870ca5ad3062028e6fe29c97163f08d
samuel <samsaras2002@gmail.com>
2025-03-16 15:03:58 +0530

    test google ads comp added

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -4,18 +4,15 @@ const AdBanner = () => {
   return (
     <div className="ad-banner">
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4005409526739627" crossorigin="anonymous"></script>
-      <amp-auto-ads type="adsense"
-        data-ad-client="ca-pub-4005409526739627">
-      </amp-auto-ads>
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="YOUR_AD_CLIENT_ID"
-        data-ad-slot="YOUR_AD_SLOT_ID"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      <ins class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-4005409526739627"
+          data-ad-slot="9126958660"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+      <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
     </div>
   );
 };
