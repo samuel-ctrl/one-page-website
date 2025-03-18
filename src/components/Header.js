accb706e6852ed4065fd7bd31ce564048a3ba1b1
samuel <samsaras2002@gmail.com>
2025-03-18 09:19:16 +0530

    gif referal ads test

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Lucky Wheel</h1>
+    <header style={{display: "flex"}}>
+      <h1 style={{marginRight: "10px", paddingBottom:"10px"}}>Lucky Wheel</h1>
+      <a href="https://beta.publishers.adsterra.com/referral/5pBNiTcQQj" rel="nofollow"><img alt="banner" src="https://landings-cdn.adsterratech.com/referralBanners/gif/120x60_adsterra_reff.gif" /></a>
     </header>
   );
 };
